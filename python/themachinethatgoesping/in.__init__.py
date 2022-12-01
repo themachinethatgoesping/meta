4d14c0284593707ad3023df086387607e765f0b9
Peter Urban <peter.urban@ugent.be>
2022-12-01 16:37:52 +0100

    file formatting in preparation for version bump

--- python/themachinethatgoesping/in.__init__.py
+++ python/themachinethatgoesping/in.__init__.py
@@ -9,63 +9,64 @@ themachinethatgoesping
 Enable quantitative processing of multibeam and singlebeam echosounder systems
 """
 
+
 @PYDEV_SRC_PATH@
 @PYDEV_MODULES@
-
 @PYDEV_INSTALL@
-
-
-#import the module functions into the current namespace
+# import the module functions into the current namespace
 __modules_installed__ = []
 
-#TODO: add warnings for installed packages with missing dependencies
+# TODO: add warnings for installed packages with missing dependencies
 
 try:
-  from themachinethatgoesping import tools
-  __modules_installed__.append(("tools",tools.__version__))
+    from themachinethatgoesping import tools
+    __modules_installed__.append(("tools", tools.__version__))
 except:
-  pass
+    pass
 
 try:
-  from themachinethatgoesping import tools_ext
-  __modules_installed__.append(("tools_ext",tools_ext.__version__))
+    from themachinethatgoesping import tools_ext
+    __modules_installed__.append(("tools_ext", tools_ext.__version__))
 except:
-  pass
+    pass
 
 try:
-  from themachinethatgoesping import navigation
-  __modules_installed__.append(("navigation",navigation.__version__))
+    from themachinethatgoesping import navigation
+    __modules_installed__.append(("navigation", navigation.__version__))
 except:
-  pass
+    pass
 
 try:
-  from themachinethatgoesping import echosounders
-  __modules_installed__.append(("echosounders",echosounders.__version__))
+    from themachinethatgoesping import echosounders
+    __modules_installed__.append(("echosounders", echosounders.__version__))
 except:
-  pass
+    pass
 
 try:
-  from themachinethatgoesping import gridding
-  __modules_installed__.append(("gridding",gridding.__version__))
+    from themachinethatgoesping import gridding
+    __modules_installed__.append(("gridding", gridding.__version__))
 except:
-  pass
+    pass
 
 # version
-#TODO this does currently not give correct results in pydev_install.enabled() mode
-#TODO this only updates the version the first time meson is executed
+# TODO this does currently not give correct results in pydev_install.enabled() mode
+# TODO this only updates the version the first time meson is executed
 __version__ = "@THEMACHINETHATGOESPING_VERSION@"
 
-#print versions
-def version():
-  width = 0
-  for module_name,version in __modules_installed__:
-    width = max(width,len(module_name))  
+# print versions
 
-  print("themachinethatgoesping")
-  print("- {:14} {}".format("version:",__version__))
-  print("\nmodules:")
-  for module,version in __modules_installed__:
-    print("- {:14} {}".format(module,version))
+
+def version():
+    width = 0
+    for module_name, version in __modules_installed__:
+        width = max(width, len(module_name))
+
+    print("themachinethatgoesping")
+    print("- {:14} {}".format("version:", __version__))
+    print("\nmodules:")
+    for module, version in __modules_installed__:
+        print("- {:14} {}".format(module, version))
+
 
 def modules():
-  return version()
+    return version()
--- python/themachinethatgoesping/in.pydev_install.py
+++ python/themachinethatgoesping/in.pydev_install.py
@@ -6,11 +6,11 @@
 import sys as __internal_module_sysy__
 from pkgutil import extend_path as __extend_path__
 
-#append path to the python source folders of themachinethatgoesping subprojects
-#each folder contains a subfolder called themachinethatgoesping
+# append path to the python source folders of themachinethatgoesping subprojects
+# each folder contains a subfolder called themachinethatgoesping
 for __submodule in pydev_install_modules:
-  __internal_module_sysy__.path.append("{}/{}/python/".format(pydev_install_src_path,__submodule))
+    __internal_module_sysy__.path.append("{}/{}/python/".format(pydev_install_src_path, __submodule))
 
-#append all paths in sys that contain themachinethatgoesping to this package
+# append all paths in sys that contain themachinethatgoesping to this package
 __path__ = __extend_path__(__path__, __name__)
 pydev_install_module_paths = __path__
